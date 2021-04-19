3a5f83dd43cd02f364b963237cea31553b692a65
levermory <lev.schepelin@icloud.com>
2021-04-19 14:37:38 +0000

    new project

---
+++ KR/DAL.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+
+namespace WpfApp1
+{
+    class DAL
+    {
+        private MySqlConnection connection = null;
+
+        public void OpenConnection(string connString)
+        {
+            connection = new MySqlConnection(connString);
+            connection.Open();
+        }
+
+        public void CloseConnection()
+        {
+            connection.Close();
+        }
+
+        public DataTable GetUsers()
+        {
+            MySqlCommand query = new MySqlCommand("select * from `user`", connection);
+           
+            DataTable restable = new DataTable();
+            using (MySqlDataReader dr = query.ExecuteReader())
+            {
+                restable.Load(dr);
+            }
+            return restable;
+        }
+        public DataTable GetMatches()
+        {
+            MySqlCommand query = new MySqlCommand("select name, name from `match`, `user` where `user`.id=`match`.us1_id and `user`.id=`match`.us2_id", connection);
+
+            DataTable restable = new DataTable();
+            using (MySqlDataReader dr = query.ExecuteReader())
+            {
+                restable.Load(dr);
+            }
+            return restable;
+        }
+        public void DeleteRow(string id)
+        {
+            MySqlCommand query = new MySqlCommand($"delete from `user` where id={id}", connection);
+            
+            query.ExecuteNonQuery();
+            
+        }
+        
+    }
+    class User
+    {
+        public int _id;
+        public string _name;
+        public int _age;
+        public string _sex;
+        public User(int id, string name, int age, string sex)
+        {
+            _id = id;
+            _name = name;
+            _age = age;
+            _sex = sex;
+        }
+
+    }
+    class Match
+    {
+        int _id;
+        int _us1_id;
+        int _us2_id;
+        public Match(int id, int us1_id, int us2_id)
+        {
+            _id = id;
+            _us1_id = us1_id;
+            _us2_id = us2_id;
+        }
+    }
+}
---
+++ KR/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+
+
+//Сайт знакомств
+//Вариант 9.
+//connected layer
+//CRUD – интерфейс без кнопок. С автоматическим доставлением
+//изменений в БД, при изменении DATAgrid.
+
+//Реализуйте возможность ввода новой строки в List<> через консоль,
+//по нажатию комбинации клавиш Ctrl + 6. Предложите пользователю в консоли
+//доставить изменения в БД. Если ответ положительный - доставьте. 
+
+
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DAL da = new DAL();
+        string connString = "server=localhost;uid=root;pwd=12345;database=tinder";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+            Console.WriteLine("hello world");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            da.OpenConnection(connString);
+            infoGrid.ItemsSource = da.GetUsers().DefaultView;
+            da.CloseConnection();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            da.OpenConnection(connString);
+            var Matches = da.GetMatches();
+            
+            infoGrid.ItemsSource = da.GetMatches().DefaultView;
+            da.CloseConnection();
+
+        }
+
+        private void infoGrid_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
+        {
+            if (infoGrid.SelectedItem != null)
+            {
+                MessageBox.Show(e.EditingElement.DataContext.ToString());
+            }
+        }
+
+        private void infoGrid_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if(infoGrid.SelectedItem != null && e.Key.ToString() == "Delete")
+            {
+                da.OpenConnection(connString);
+                da.DeleteRow((infoGrid.SelectedItems[0] as DataRowView).Row.ItemArray.GetValue(0).ToString());
+                da.CloseConnection();
+            }
+        }
+    }
+}
